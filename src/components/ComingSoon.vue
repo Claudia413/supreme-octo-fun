83e5edcecc7109b28b558f3a522a8c0e0c8f67d1
Claudia <cmjengelsman@gmail.com>
2019-02-17 16:24:57 +0100

    Create coming soon page to be launched as placeholder website

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,88 @@
+<template>
+	<header>
+		<h2 class="developer">Coming soon!</h2>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "ComingSoon"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../assets/_variables.scss";
+
+header {
+	max-width: 100%;
+	background-color: $primary-color;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	h2 {
+		display: inline-block;
+		background: linear-gradient(
+			to top right,
+			#ffddaa 0%,
+			#fcecc4 10%,
+			#fcda7f 20%,
+			#fde799 33% #ffddaa 43%,
+			#fcecc4 53%,
+			#fcda7f 60%,
+			#fde799 66% #ffddaa 76%,
+			#fcecc4 86%,
+			#fcda7f 96%,
+			#fde799 100%
+		);
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+		margin: 6%;
+		background-size: 800% 800%;
+
+		-webkit-animation: MetallicBackground 15s ease infinite;
+		-moz-animation: MetallicBackground 15s ease infinite;
+		animation: MetallicBackground 15s ease infinite;
+
+		@-webkit-keyframes MetallicBackground {
+			0% {
+				background-position: 0% 50%;
+			}
+			50% {
+				background-position: 100% 50%;
+			}
+			100% {
+				background-position: 0% 50%;
+			}
+		}
+		@-moz-keyframes MetallicBackground {
+			0% {
+				background-position: 0% 50%;
+			}
+			50% {
+				background-position: 100% 50%;
+			}
+			100% {
+				background-position: 0% 50%;
+			}
+		}
+		@keyframes MetallicBackground {
+			0% {
+				background-position: 0% 50%;
+			}
+			50% {
+				background-position: 100% 50%;
+			}
+			100% {
+				background-position: 0% 50%;
+			}
+		}
+		&.developer {
+			font-size: 64px;
+			font-family: "Raleway", sans-serif;
+			animation-delay: 1.5s;
+		}
+	}
+}
+</style>
