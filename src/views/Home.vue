bb7d8755fe5c134aef668aac32eb377afe3a2da1
Claudia <cmjengelsman@gmail.com>
2019-02-19 08:19:24 +0100

    Add new content and another block for the homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,8 @@
 		<img
 			alt="Claudia Engelsman, developer who will teach you how to code a website"
 			src="../assets/Claudia.jpg"
-			class="intro-pic"
-		>
-		<section class="intro-text">
+			class="intro-pic img-block">
+		<section class="intro-text text-block">
 			<h3>About</h3>
 			<p>A business graduate turned developer. That is me, Claudia Engelsman. Switching from a job in purchasing to a developer job didn’t happen overnight, but I was earning a salary as a developer within a month of quitting and without any experience. I will help you to do the same by sharing everything I’ve learned, for free, and help you find inspiration and stay motivated to start creating your own websites.</p>
 			<button class="mt-2">More about me</button>
@@ -15,15 +14,27 @@
 			<div class="arrow" @click="incrementSlideIndex(-1)">
 				<span class="fas fa-chevron-left"></span>
 			</div>
-			<transition name="fade" mode="out-in">
-      	<p class="inspiration" v-if="slideIndex === 1" key="one">Hello test</p>
-      	<p class="inspiration" v-if="slideIndex === 2" key="two">I should be visible</p>
-				<p class="inspiration" v-if="slideIndex === 3" key="three">Me too!</p>
-			</transition>
+			<div class="slider">
+				<h4>Now is the perfect time to start because:</h4>
+				<transition name="fade" mode="out-in">
+					<p class="inspiration" v-if="slideIndex === 1" key="one">It has never been easier to build websites. Not kidding, technology is quickly improving the ease and fun of building websites, making this an excellent time to try it (again). </p>
+					<p class="inspiration" v-if="slideIndex === 2" key="two">There is a huge shortage of developers and this amount is only growing. More jobs than developers means developers have the upperhand in job negotiations. Sounds good huh!</p>
+					<p class="inspiration" v-if="slideIndex === 3" key="three">Building websites can be done from any computer with a solid internet connection. Working from home, working remotely from a hammock, anything is possible.</p>
+				</transition>
+			</div>
 			<div class="arrow" @click="incrementSlideIndex(1)">
 				<span class="fas fa-chevron-right"></span>
 			</div>
-    </section>
+		</section>
+		<img
+			alt="Claudia Engelsman, developer who will teach you how to code a website"
+			src="../assets/Claudia.jpg"
+			class="try-it-pic img-block">
+		<section class="try-it-text text-block">
+			<h3>Try it</h3>
+			<p>Curious about building websites but don't know where to start?Feeling overwhelmed by all the information out there? Try out my guide with step by step articles, and practise, and I'll help you land a job as a webdeveloper in no time!</p>
+			<button class="mt-2">To the guide</button>
+		</section>
 		<Footer class="footer"/>
 	</div>
 </template>
@@ -34,22 +45,22 @@ import Footer from "@/components/Footer.vue";
 import HeaderAnimation from "@/components/HeaderAnimation.vue";
 
 export default {
-  name: "home",
-  data () {
-    return {
+	name: "home",
+	data() {
+		return {
 			slideIndex: 1,
 			numberOfSlides: 3
-    }
-  },
+		};
+	},
 	components: {
 		Footer,
 		HeaderAnimation
 	},
 	methods: {
 		incrementSlideIndex(number) {
-			if(this.slideIndex + number > this.numberOfSlides) {
+			if (this.slideIndex + number > this.numberOfSlides) {
 				this.slideIndex = 1;
-			}else if(this.slideIndex + number > 0){
+			} else if (this.slideIndex + number > 0) {
 				this.slideIndex = this.slideIndex + number;
 			} else {
 				this.slideIndex = this.numberOfSlides;
@@ -67,11 +78,12 @@ export default {
 	display: grid;
 	grid-gap: 0;
 	grid-template-columns: repeat(2, 50%);
-	grid-template-rows: auto 400px auto;
+	grid-template-rows: auto 400px auto 400px;
 	grid-template-areas:
 		"header header"
-    "intro-pic intro-text"
-    "slider slider"
+		"intro-pic intro-text"
+		"slider slider"
+		"try-it-text try-it-pic"
 		"footer footer";
 	@media only screen and (max-width: 768px) {
 		grid-template-columns: repeat(2, 50%);
@@ -79,8 +91,10 @@ export default {
 		grid-template-areas:
 			"header header"
 			"intro-pic intro-pic"
-      "intro-text intro-text"
-      "slider slider"
+			"intro-text intro-text"
+			"slider slider"
+			"try-it-pic try-it-pic"
+			"try-it-text try-it-text"
 			"footer footer";
 	}
 	.mt-2 {
@@ -93,51 +107,71 @@ export default {
 }
 .intro-pic {
 	grid-area: intro-pic;
+}
+.intro-text {
+	grid-area: intro-text;
+}
+.slideit {
+	grid-area: slider;
+	background-color: $secondary-color;
+	height: 250px;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	font-family: 'Abel', sans-serif;
+	.arrow {
+			font-size: 24px;
+			cursor: pointer;
+			height: 100%;
+			width: 40px;
+			display: flex;
+			.fas {
+				color: #808080;
+				margin: auto;
+			}
+		}
+	.slider {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+		opacity: 1;
+		max-width: 70%;
+		text-align: center;
+		.fade-enter-active {
+			animation: fade-in 0.3s;
+		}
+		.fade-leave-active {
+			animation: fade-in 0.3s reverse;
+		}
+		@keyframes fade-in {
+			0% {
+				opacity: 0;
+			}
+			100% {
+				opacity: 1;
+			}
+		}
+	}
+}
+.try-it-pic {
+	grid-area: try-it-pic;
+}
+.try-it-text {
+	grid-area: try-it-text;
+}
+.text-block {
+	text-align: justify;
+	padding: 40px;
+}
+.img-block {
 	width: 100%;
 	height: 100%;
 	object-fit: cover;
 	@media only screen and (max-width: 768px) {
 		height: 400px;
 		max-width: 100vw;
-	}
-}
-.intro-text {
-	grid-area: intro-text;
-	text-align: justify;
-	padding: 40px;
-}
-.slideit {
-  grid-area: slider;
-  background-color: $secondary-color;
-	height: 250px;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	opacity: 1;
-	.arrow {
-		font-size: 24px;
-		margin: 20px;
-		cursor: pointer;
-		height: 100%;
-		width: 40px;
-		display: flex;
-		.fas{
-			margin: auto;
-		}
-	}
-	.fade-enter-active {
-  	animation: fade-in 0.3s;
-	}
-	.fade-leave-active {
-  	animation: fade-in .3s reverse;
-	}
-	@keyframes fade-in {
-  	0% {
-    	opacity: 0;
-  	}
-  	100% {
-    	opacity: 1;
-  	}
 	}
 }
 .footer {
