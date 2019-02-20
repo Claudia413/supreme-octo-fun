49822d7ad67766c475ebb2ee73af28f6c1f2ca32
Claudia <cmjengelsman@gmail.com>
2019-02-20 08:15:42 +0100

    Add  setup for menu on desktop

---
+++ src/components/MenuSlide.vue
@@ -0,0 +1,27 @@
+<template>
+<transition name="slide-in">
+	<div>
+		<!-- left part with icon and triangles?  -->
+	</div>
+	<div class="menu-list">
+		<span>Home</span>
+		<span>About</span>
+		<span>Guide</span>
+		<span>Blog</span>
+		<span>Contact</span>
+		<!-- Icons here -->
+	</div>
+</transition>
+</template>
+
+<script>
+export default {
+	name: "MenuSlide"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../assets/_variables.scss";
+
+</style>
