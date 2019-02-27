40bd1a6ef5936795fa052443da32736eb11ec9fb
Claudia <cmjengelsman@gmail.com>
2019-02-27 17:48:43 +0100

    Add about text

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,7 +9,18 @@
 			<h3>About</h3>
 			<p>Hello text here etc</p>
 		</section>
-    <Footer class="footer"/>
+		<section class="full-block long-bio">
+      <h3>About</h3>
+			<p>A few years ago I turned my life around and started over at age 26. I had just moved back in with my parents after a break-up when I realized how unfulfilling my current career path was. I wanted to become a developer and I quit my job without a new job or even a solid plan to get there. Uh-oh...</p>
+			<p>I started reading about and trying out HTML and CSS online, going from one free resource to the next. There is so much information available, but not all is correct, up to date or complete, I was lost and just went with everything I found.</p>
+			<p>Fast forward a few weeks, a dash of luck and I got an interview with my current employer. They took a huge chance on this honest and unexperienced girl and decided to hire me and teach me coding on the job. I felt like the stars aligned just for me and I was so excited to start.</p>
+			<p>Since then I’ve never been more frustrated with work problems and errors and such, but I also have never been happier when I do figure it out and a new feature is build for one of our websites. It is such a thrill to create something out of thin air and help so many people with it.</p>
+			<p>Now that I’m here, I remember I had such a hard and confusing time in the beginning of this journey, and I feel like I should improve this for others who want to learn how to code. This is where and when I decided to create a space with all the information to get started, with support and inspiration to keep going when it gets hard, or when you are frustrated. A place I wish I had back then, starting with all of this.</p>
+			<p>My goal is to be the ‘aha-moment’ for someone struggling with learning all this on their own. To be the support for someone thinking of giving up while they are so close. And to be the inspiration for someone who wants to learn coding but is afraid to get started. If there is one person today I am helping with this website, it is a good day.</p>
+			<p>When I’m not working as a developer or creating guides and providing help, you can find me on the couch in yoga pants with my pupper and some B&J ice cream, dreaming of New Zealand or simply (binge)watching Netflix. I also enjoy watercolor painting, and I don’t make enough time for this. Hopefully one day I will master time-management and have enough time for all my (creative) interests and endeavours.</p>
+		</section>
+
+		<Footer class="footer"/>
 	</div>
 </template>
 
@@ -17,10 +28,10 @@
 import Footer from "@/components/Footer.vue";
 
 export default {
-  components: {
+	components: {
 		Footer
-	},
-}
+	}
+};
 </script>
 
 
@@ -35,7 +46,7 @@ export default {
 	grid-template-rows: 600px auto;
 	grid-template-areas:
 		"intro-pic intro-text"
-		"slider slider"
+    "long-bio long-bio"
 		"footer footer";
 	@media only screen and (max-width: 768px) {
 		grid-template-columns: repeat(2, 50%);
@@ -43,7 +54,7 @@ export default {
 		grid-template-areas:
 			"intro-pic intro-pic"
 			"intro-text intro-text"
-			"slider slider"
+			"long-bio long-bio"
 			"footer footer";
 	}
 	.mt-2 {
@@ -57,21 +68,16 @@ export default {
 .intro-text {
 	grid-area: intro-text;
 }
-.img-block {
-	width: 100%;
-	height: 100%;
-	object-fit: cover;
-	@media only screen and (max-width: 768px) {
-		height: 400px;
-		max-width: 100vw;
-	}
-}
-.text-block {
-	text-align: justify;
-	padding: 40px;
-	p {
-		max-width: 600px;
-	}
+.long-bio {
+  grid-area: long-bio;
+  display: flex;
+  flex-direction: column;
+  padding: 40px;
+  background-color: $secondary-color;
+  p {
+    width: 80%;
+    max-width: 1400px;
+  }
 }
 .footer {
 	grid-area: footer;
