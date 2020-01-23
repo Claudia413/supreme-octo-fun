60b8102ef61606e3486292c9866725d22d470a19
Claudia <cmjengelsman@gmail.com>
2020-01-24 08:57:32 +1300

    Work in progress on layout of blog posts

--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -1,25 +1,22 @@
 <template>
-	<div class="overview">
-		<HeaderAnimation class="headeranimation">
-            <template v-slot:title>
-				<h1>Welcome to the blog you selected!</h1>
-			</template>
-		</HeaderAnimation>
+	<div class="blog">
 		<MenuSlide/>
-        <prismic-rich-text :field="blog.title"/>
-        <prismic-image :field="blog.header" class="blog header"/>
+        <header>
+            <prismic-image :field="blog.header" class="cover"/>
+            <prismic-rich-text :field="blog.title" class="title"/>
+        </header>
         <prismic-rich-text :field="blog.subtitle"/>
-    <section v-for="(slice, index) in slices" :key="'slice-' + index">
-      <template v-if="slice.slice_type === 'blog_text_block'">
-        <prismic-rich-text :field="slice.primary.section_title"/>
-        <prismic-rich-text :field="slice.primary.section_text"/>
+        <section v-for="(slice, index) in slices" :key="'slice-' + index" class="content">
+        <template v-if="slice.slice_type === 'blog_text_block'">
+            <prismic-rich-text :field="slice.primary.section_title"/>
+            <prismic-rich-text :field="slice.primary.section_text"/>
 
-      </template>
-      <template v-else-if="slice.slice_type === 'image_with_caption'">
-        <prismic-rich-text :field="slice.primary.image_description"/>
-        <prismic-image :field="slice.primary.image" />
-      </template>
-    </section>
+        </template>
+        <template v-else-if="slice.slice_type === 'image_with_caption'">
+            <prismic-rich-text :field="slice.primary.image_description"/>
+            <prismic-image :field="slice.primary.image" />
+        </template>
+        </section>
 	</div>
 </template>
 
@@ -41,7 +38,6 @@ export default {
 		};
 	},
 	components: {
-		HeaderAnimation,
 		MenuSlide,
 	},
 	methods: {
@@ -53,7 +49,6 @@ export default {
                 this.blog.subtitle = document.data.subtitle
                 this.blog.header = document.data.blog_image
                 this.slices = document.data.body
-            console.log('this.blog', this.blog );
             });
         },
 	},
@@ -67,9 +62,47 @@ export default {
 @import "../assets/_variables.scss";
 
 .blog {
-  .header {
-    max-width: 100%;
-  }
+    background-color: $body-bg;
+    .content {
+        width: 100%;
+        max-width: 1400px;
+        h2 {
+            color: $primary-bright;
+            font-family: 'Spectral', sans-serif;
+            line-height:39px;
+            font-size: 23px;
+            }
+        p {
+            line-height: 26px;
+        }
+    }
+}
+
+header {
+    position: relative;
+    .cover {
+        grid-area: header;
+        height: 50vh;
+        object-fit: cover;
+        width: 100%;
+        max-width: 1400px;
+        object-position: center;
+        margin-left: auto;
+        margin-right: auto;
+        display: block;
+    }
+    .title {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
 }
 
 </style>
