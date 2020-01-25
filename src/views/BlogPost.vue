80fa9b4fc897462fbed78b440addec52fb3967e3
Claudia <cmjengelsman@gmail.com>
2020-01-25 19:06:05 +1300

    Add more styling for the blogpost page

--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -1,60 +1,66 @@
 <template>
 	<div class="blog">
-		<MenuSlide/>
-        <header>
-            <prismic-image :field="blog.header" class="cover"/>
-            <prismic-rich-text :field="blog.title" class="title"/>
-        </header>
-        <prismic-rich-text :field="blog.subtitle"/>
-        <section v-for="(slice, index) in slices" :key="'slice-' + index" class="content">
-        <template v-if="slice.slice_type === 'blog_text_block'">
-            <prismic-rich-text :field="slice.primary.section_title"/>
-            <prismic-rich-text :field="slice.primary.section_text"/>
-
-        </template>
-        <template v-else-if="slice.slice_type === 'image_with_caption'">
-            <prismic-rich-text :field="slice.primary.image_description"/>
-            <prismic-image :field="slice.primary.image" />
-        </template>
-        </section>
+		<MenuSlide />
+		<div class="blog-grid">
+			<header>
+				<prismic-image :field="blog.header" class="cover" />
+			</header>
+			<div class="content">
+				<h1 class="title">{{blog.title[0].text}}</h1>
+				<prismic-rich-text :field="blog.subtitle" />
+				<section v-for="(slice, index) in slices" :key="'slice-' + index">
+					<template v-if="slice.slice_type === 'blog_text_block'">
+						<h2 class="heading" >{{slice.primary.section_title[0].text}}</h2>
+						<prismic-rich-text :field="slice.primary.section_text" class="text"/>
+					</template>
+					<template v-else-if="slice.slice_type === 'image_with_caption'">
+						<prismic-image :field="slice.primary.image" class="blog-image" />
+						<prismic-rich-text :field="slice.primary.image_description" />
+					</template>
+				</section>
+			</div>
+			<aside>
+				<p>short bio here</p>
+				<p>links here</p>
+			</aside>
+		</div>
 	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HeaderAnimation from "@/components/HeaderAnimation.vue";
 import MenuSlide from "@/components/MenuSlide.vue";
 
 export default {
 	name: "blogPost",
 	data() {
 		return {
-            blog: {
-                title: [],
-                subtitle: [],
-                header: {}
-            },
-            slices: []
+			blog: {
+				title: [],
+				subtitle: [],
+				header: {}
+			},
+			slices: []
 		};
 	},
 	components: {
-		MenuSlide,
+		MenuSlide
 	},
 	methods: {
-
 		getContent(uid) {
-            this.$prismic.client.getByUID('blogpost', uid).then((document) => {
-                // Can't simply put the result in an empty blog object without console erros about undefined before its done loading. So we set them individually with starting empty objects and arrays
-                this.blog.title = document.data.title
-                this.blog.subtitle = document.data.subtitle
-                this.blog.header = document.data.blog_image
-                this.slices = document.data.body
-            });
-        },
+			this.$prismic.client.getByUID("blogpost", uid).then(document => {
+				// Can't simply put the result in an empty blog object without console erros about undefined before its done loading. So we set them individually with starting empty objects and arrays
+				this.blog.title = document.data.title;
+				this.blog.subtitle = document.data.subtitle;
+				this.blog.header = document.data.blog_image;
+                this.slices = document.data.body;
+                console.log('slices', this.slices);
+			});
+		}
 	},
-	created () {
-    this.getContent(this.$route.params.uid);
-  }
+	created() {
+		this.getContent(this.$route.params.uid);
+	}
 };
 </script>
 
@@ -62,47 +68,78 @@ export default {
 @import "../assets/_variables.scss";
 
 .blog {
-    background-color: $body-bg;
-    .content {
-        width: 100%;
-        max-width: 1400px;
-        h2 {
+	background-color: $body-bg;
+	.blog-grid {
+		display: grid;
+		grid-gap: 0;
+		grid-template-columns: 80fr 20fr;
+		grid-template-rows: 50vh auto;
+		grid-template-areas:
+			"header header"
+			"content aside";
+		@media only screen and (max-width: 768px) {
+			grid-template-areas:
+				"header header"
+				"content content"
+				"aside aside";
+		}
+	}
+	.content {
+		grid-area: content;
+		width: 100%;
+		max-width: 900px;
+		margin-left: auto;
+		margin-right: auto;
+		display: flex;
+        flex-direction: column;
+        .title {
             color: $primary-bright;
-            font-family: 'Spectral', sans-serif;
-            line-height:39px;
-            font-size: 23px;
-            }
-        p {
-            line-height: 26px;
+            text-align: center;
+            padding: 24px 12px;
         }
-    }
+		.heading {
+			color: $primary-bright;
+			font-family: "Spectral", sans-serif;
+			line-height: 39px;
+			font-size: 23px;
+        }
+        section {
+            padding: 12px;
+            @media only screen and (max-width: 768px) {
+                padding: 12px 60px 12px 12px;
+            }
+        }
+		p {
+			line-height: 26px;
+		}
+		.blog-image {
+			max-width: 100%;
+        }
+        .text {
+            text-align: justify;
+        }
+	}
+	aside {
+		grid-area: aside;
+		width: 100%;
+		max-width: 400px;
+	}
 }
 
 header {
-    position: relative;
-    .cover {
-        grid-area: header;
-        height: 50vh;
-        object-fit: cover;
-        width: 100%;
-        max-width: 1400px;
-        object-position: center;
-        margin-left: auto;
-        margin-right: auto;
-        display: block;
-    }
-    .title {
-        height: 100%;
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-    }
+	grid-area: header;
+	position: relative;
+	.cover {
+		grid-area: header;
+		height: 50vh;
+		object-fit: cover;
+		width: 100%;
+		max-width: 1400px;
+		object-position: center;
+		margin-left: auto;
+		margin-right: auto;
+		display: block;
+	}
 }
 
 </style>
