38b2fa4517ebb929e3b8a1947be61b8f98c838c5
Claudia <cmjengelsman@gmail.com>
2020-03-26 17:26:14 +1300

    Turn of prerendering for now as test since Netlify says its doing that now

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,3 @@
-const PrerenderSpaPlugin = require('prerender-spa-plugin')
-// const Renderer = PrerenderSpaPlugin.PuppeteerRenderer
-const path = require('path')
-
 module.exports = {
     runtimeCompiler: true
 }
