f2c6a01f088cef9103c13fc158b5261afe8ef731
Claudia <cmjengelsman@gmail.com>
2020-04-08 12:07:41 +1200

    Add load more blogs functionality to NZ posts also

--- src/views/BlogOverview.vue
+++ src/views/BlogOverview.vue
@@ -33,7 +33,7 @@
                 </div>
                 <BlogPreview v-else v-for="(post, index) in blogpreviewstech" :key="'post-' + index" :blogId="post.uid" class="blog-post" :image="post.data.blog_image" :title="post.data.title[0].text" />
             </div>
-            <p :class="maxPageNumber>pageNumber?'':'hidden'" @click="showMoreTechBlogPosts()"> More blogs >> </p>
+            <p :class="maxPageNumber>pageNumber? '':'hidden'" @click="showMoreTechBlogPosts()"> More blogs >> </p>
 		</section>
         <!-- <section class="featured-blog">
             <h3>Featured post</h3>
@@ -49,16 +49,17 @@
                     <div class="blog-pic"></div>
                     <div class="blog-tag-text"></div>
                 </div>
-                <!-- <!-- <div v-if="$store.state.loadingBlogsNZ" class="blog-post placeholder">
+                 <div v-if="$store.state.loadingBlogsNZ" class="blog-post placeholder">
                     <div class="blog-pic"></div>
                     <div class="blog-tag-text"></div>
-                </div> -->
+                </div>
                 <div v-if="$store.state.loadingBlogsNZ" class="blog-post placeholder">
                     <div class="blog-pic"></div>
                     <div class="blog-tag-text"></div>
                 </div>
                 <BlogPreview v-else v-for="(post, index) in blogpreviewsnz" :key="'post-' + index" :blogId="post.uid" class="blog-post" :image="post.data.blog_image" :title="post.data.title[0].text" />
             </div>
+            <p :class="maxPageNumberNZ>pageNumberNZ? 'light-bg':'hidden'" @click="showMoreNZBlogPosts()"> More blogs >> </p>
 		</section>
 	</div>
 </template>
@@ -75,7 +76,9 @@ export default {
 	data() {
 		return {
             pageNumber: 1,
-            maxPageNumber: 2
+            maxPageNumber: 2,
+            pageNumberNZ: 1,
+            maxPageNumberNZ: 2,
 		};
 	},
 	components: {
@@ -99,15 +102,16 @@ export default {
             // response is the response object, response.results holds the documents
 			});
         },
-        getContentNZ() {
+        getContentNZ(pageNumberNZ) {
 			this.$prismic.client.query([
             this.$prismic.Predicates.at('document.type', 'blogpost'),
             this.$prismic.Predicates.at('document.tags', ['NZ'])],
 			{
 			orderings : '[document.first_publication_date desc]',
-			pageSize : 4,
-			fetch : ['blogpost.title', 'blogpost.blog_image'] }
-            ).then((response) => {
+            pageSize : 4,
+            page: pageNumberNZ,
+			fetch : ['blogpost.title', 'blogpost.blog_image'] }).then((response) => {
+                this.maxPageNumberNZ = response.total_pages
                 this.$store.dispatch('setNZBlogPreviewsFromPrismic', response.results);
 			// response is the response object, response.results holds the documents
 			});
@@ -115,6 +119,10 @@ export default {
         showMoreTechBlogPosts() {
             this.pageNumber = this.pageNumber + 1;
             this.getContentTech(this.pageNumber);
+        },
+        showMoreNZBlogPosts() {
+            this.pageNumberNZ = this.pageNumberNZ + 1;
+            this.getContentNZ(this.pageNumberNZ);
         }
     },
     computed: mapState([
@@ -123,7 +131,7 @@ export default {
         ]),
 	created () {
         if (this.$store.state.blogpreviewsnz.length === 0) {
-            this.getContentNZ();
+            this.getContentNZ(this.pageNumberNZ);
         }
         if (this.$store.state.blogpreviewstech.length === 0) {
             this.getContentTech(this.pageNumber);
@@ -159,6 +167,9 @@ export default {
         cursor: pointer;
         font-size: 16px;
         transition: opacity 0.4s, visibility 0.4s ease-in-out;
+        &.light-bg {
+            color: inherit;
+        }
         &.hidden {
             opacity: 0;
             visibility: hidden;
