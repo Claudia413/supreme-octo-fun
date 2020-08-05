2d54d1980595d3c00649d38424449673e085a01e
Claudia <cmjengelsman@gmail.com>
2020-08-05 16:24:20 +1200

    Copy and basicstyling, needs improvement but done is better than perfect

--- src/views/HireMe.vue
+++ src/views/HireMe.vue
@@ -14,11 +14,27 @@
 		>
 		<section class="intro-text text-block">
 			<h3>Hi there!</h3>
-			<p>Good to e-meet you. </p>
+			<p>Curious to find out what we can build together? As a developer and adventurer I can always create custom work, but just to give you an idea of what I can do I've listed some of my favorite ways to help you below.</p>
 		</section>
-		<section class="full-block long-bio">
+		<section class="full-block services">
 			<h3>How can I help you?</h3>
-			<p>List services</p>
+			<h4>Small businesses</h4>
+			<p>Create a website for you business:</p>
+			<ul>
+				<li>A website that is responsive and looks good on mobile phones, tablets and desktops</li>
+				<li>Optimize for search engines so your new website can be found and read by search engines</li>
+				<li>A website for your business that you can maintain yourself if you like</li>
+				<li>Optimize for accessibility to ensure all your customers, even those with a disability can access the information they need</li>
+				<li>A website that links to your social media accounts</li>
+				<li>A safe and technically sound website that does not require much maintenance</li>
+			</ul>
+			<h4>Code for a Cause</h4>
+			<p>I believe in helping others and for that reason I have set up this Code for a Cause project. I will help non-profit organizations with their website for half a day per week. Currently I have only one opening for a non-profit left in my schedule so get in touch as soon as possible!</p>
+			<h4>Hire me</h4>
+			<p>For the right company I can be more than a project based freelancer, with the right company I'd love to commit and be an employee working with your team. Get in touch and let's grab a coffee/tea together!</p>
+			<p>Some of my favorite skills are VueJs, Javascript, HTML5, CSS3. I also have experience with Prismic.io (API CMS), React, Gatsby, PHP (Laravel), Scrum and Holocracy, and a lot more abbreviations ;-)</p>
+			<h4>Other enquiries</h4>
+			<p>Want to work together on something else? Just reach out, I love getting mail that's not a newsletter or spam so I'll be excited to hear from you!</p>
 		</section>
 
 
@@ -69,7 +85,7 @@ export default {
 .intro-text {
 	grid-area: intro-text;
 }
-.long-bio {
+.services {
 	grid-area: long-bio;
 	display: flex;
 	flex-direction: column;
@@ -77,26 +93,40 @@ export default {
 	background-color: $dark-blue;
 	text-align: center;
 	h3 {
+		color: $text-darkbg;
+		margin-bottom: 20px;;
+	}
+	h4 {
+		color: white;
 		color: $light-blue;
+		font-family: 'Karla', sans-serif;
+		text-transform: none;
+		font-weight: 500;
+		margin-bottom: 0;
+		margin-top: 20px;
 	}
 	p {
 		color: $text-darkbg;
 		width: 80%;
 		max-width: 1400px;
-		&.highlight {
-			font-size: 18px;
-			font-style: italic;
-			color: $light-blue;
-			padding: 12px 0;
-			font-weight: 500;
-		}
+	}
+	ul {
+		font-size: 14px;
+		margin: 0;
+		text-align: left;
+		max-width: 75%;
+	}
+	li {
+		color: $text-darkbg;
+		list-style-type: circle;
+		padding-block-start: 4px;
+		padding-block-end: 4px;
+		list-style-position: inside;
 	}
 	@media only screen and (max-width: 768px) {
 		padding: 80px 40px;
 	}
 }
-.did-you-know {
-	grid-area: did-you-know;
-}
+
 </style>
 
