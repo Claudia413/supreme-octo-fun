c134311137993596789dbcc5bf9f8b94611d2e73
Claudia <cmjengelsman@gmail.com>
2021-12-29 11:54:31 +1300

    Replace text with aria labels for FA icons, make css unscoped

--- src/components/SocialIconBar.vue
+++ src/components/SocialIconBar.vue
@@ -1,56 +1,68 @@
 <template>
-	<div class="icon-bar">
-		<a v-if="instagram" href="http://instagram.com/claudia414" target="_blank" rel="noreferrer" class="hex-background">
-			<i class="fab fa-instagram"> Instagram</i>
-		</a>
-		<a v-if="pinterest" href="https://pinterest.com/ClaudiaEngelsman/" target="_blank" rel="noopener" class="hex-background">
-			<i class="fab fa-pinterest"> Pinterest</i>
-		</a>
-		<a v-if="linkedIn" href="https://www.linkedin.com/in/claudia-engelsman/" target="_blank" rel="noreferrer" class="hex-background">
-			<i class="fab fa-linkedin"> LinkedIn</i>
-		</a>
-        <a v-if="twitter" href="https://twitter.com/claudia_blonde" target="_blank" rel="noreferrer" class="hex-background">
-            <i class="fab fa-twitter"> Twitter</i>
-        </a>
-        <a v-if="website" href="https://www.claudiaengelsman.com" target="_blank" rel="noreferrer" class="hex-background">
-            <i class="fas fa-globe-europe"> Website</i>
-        </a>
-	</div>
+  <div class="icon-bar">
+    <a v-if="instagram" href="http://instagram.com/claudia414" target="_blank" rel="noreferrer" class="hex-background">
+      <i aria-label="Instagram" class="fab fa-instagram"></i>
+    </a>
+    <a
+      v-if="pinterest"
+      href="https://pinterest.com/ClaudiaEngelsman/"
+      target="_blank"
+      rel="noopener"
+      class="hex-background"
+    >
+      <i aria-label="Pinterest" class="fab fa-pinterest"></i>
+    </a>
+    <a
+      v-if="linkedIn"
+      href="https://www.linkedin.com/in/claudia-engelsman/"
+      target="_blank"
+      rel="noreferrer"
+      class="hex-background"
+    >
+      <i aria-label="LinkedIn" class="fab fa-linkedin"></i>
+    </a>
+    <a v-if="twitter" href="https://twitter.com/claudia_blonde" target="_blank" rel="noreferrer" class="hex-background">
+      <i aria-label="Twitter" class="fab fa-twitter"></i>
+    </a>
+    <a v-if="website" href="https://www.claudiaengelsman.com" target="_blank" rel="noreferrer" class="hex-background">
+      <i aria-label="Website" class="fas fa-globe-europe"></i>
+    </a>
+  </div>
 </template>
 
 <script>
-export default {
+  export default {
     name: "SocialIconBar",
     props: {
-		instagram: {
-			type: Boolean,
-			default: true
-		},
-		linkedIn: {
-			type: Boolean,
-			default: true
-		},
-		pinterest: {
-			type: Boolean,
-			default: true
-        },
-        twitter: {
-            type: Boolean,
-            default: true
-        },
-        website: {
-            type: Boolean,
-            default: false
-        }
-	},
-};
+      instagram: {
+        type: Boolean,
+        default: true,
+      },
+      linkedIn: {
+        type: Boolean,
+        default: true,
+      },
+      pinterest: {
+        type: Boolean,
+        default: true,
+      },
+      twitter: {
+        type: Boolean,
+        default: true,
+      },
+      website: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-@import "../assets/_variables.scss";
+<style lang="scss">
+  @import "../assets/_variables.scss";
 
-.icon-bar {
+  .icon-bar {
     display: flex;
     width: 156px;
     flex-direction: row;
@@ -58,50 +70,54 @@ export default {
     justify-content: space-between;
     flex-wrap: wrap;
     .hex-background {
-        height: 48px;
-        width: 48px;
-        background-image: url("./../assets/hexagonal.svg");
-        background-position: center;
-        background-repeat: no-repeat;
-        background-origin: border-box;
-        background-size: contain;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-weight: 500;
-        margin: auto;
-        &:hover, &:focus {
-            .fab, .fas {
-                transition: 0.3s all ease;
-                opacity: 1;
-            }
+      height: 48px;
+      width: 48px;
+      background-image: url("./../assets/hexagonal.svg");
+      background-position: center;
+      background-repeat: no-repeat;
+      background-origin: border-box;
+      background-size: contain;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-weight: 500;
+      margin: auto;
+      &:hover,
+      &:focus {
+        .fab,
+        .fas {
+          transition: 0.3s all ease;
+          opacity: 1;
         }
+      }
     }
-    .fab, .fas {
-        font-size: 24px;
-        width: 23px;
-        height: 23px;
-        background: -webkit-linear-gradient(left top, #FEDB37, #FDB931, #9f7928, #8A6E2F);
-        background: -webkit-linear-gradient(left top, #a66251, #facec8, #a26050, #faf3f3);
-        background: -webkit-linear-gradient(left top, #ac6943, #fbd1c1, #ab6943, #faf4ed);
-        opacity: 0.9;
+    .fab,
+    .fas {
+      background-repeat: no-repeat;
+      font-size: 24px;
+      width: 23px;
+      height: 23px;
+      background: -webkit-linear-gradient(left top, #fedb37, #fdb931, #9f7928, #8a6e2f);
+      background: -webkit-linear-gradient(left top, #a66251, #facec8, #a26050, #faf3f3);
+      background: -webkit-linear-gradient(left top, #ac6943, #fbd1c1, #ab6943, #faf4ed);
+      opacity: 0.9;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      transition: 0.3s all ease;
+      &:hover,
+      &focus {
+        transition: 0.3s all ease;
+        opacity: 1;
+        // background: -webkit-linear-gradient(left top, #FFFFAC, #FEDB37, #FDB931, #5d4a1f);
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
-        transition: 0.3s all ease;
-        &:hover, &focus {
-            transition: 0.3s all ease;
-            opacity: 1;
-            // background: -webkit-linear-gradient(left top, #FFFFAC, #FEDB37, #FDB931, #5d4a1f);
-            -webkit-background-clip: text;
-            -webkit-text-fill-color: transparent;
-        }
+      }
     }
     .fa-instagram {
-        margin-left: 2px;
+      margin-left: 2px;
     }
     @media only screen and (max-width: 768px) {
-        flex-wrap: wrap-reverse;
+      flex-wrap: wrap-reverse;
     }
-}
-
+  }
 </style>
