019c445081ccf3949afe073ca1791f905b85b29f
Claudia <cmjengelsman@gmail.com>
2022-10-30 16:46:48 +1300

    Add 2 projects to portfolio to start with, add portfolio to menu

--- src/components/MenuSlide.vue
+++ src/components/MenuSlide.vue
@@ -9,7 +9,7 @@
       <router-link to="/about">About</router-link>
       <!-- <router-link to="/guide">Guide</router-link> -->
       <router-link to="/blog">Blog</router-link>
-      <!-- <router-link to="/portfolio">Portfolio</router-link> -->
+      <router-link to="/portfolio">Portfolio</router-link>
       <router-link to="/work-with-me">Work with me</router-link>
       <SocialIconBar></SocialIconBar>
     </nav>
@@ -17,50 +17,89 @@
 </template>
 
 <script>
-import SocialIconBar from "@/components/SocialIconBar.vue";
+  import SocialIconBar from "@/components/SocialIconBar.vue";
 
-export default {
-  name: "MenuSlide",
-  data() {
-    return {
-      menuOpen: false,
-    };
-  },
-  components: {
-    SocialIconBar,
-  },
-  methods: {
-    toggleMenu() {
-      this.menuOpen = !this.menuOpen;
+  export default {
+    name: "MenuSlide",
+    data() {
+      return {
+        menuOpen: false,
+      };
     },
-  },
-};
+    components: {
+      SocialIconBar,
+    },
+    methods: {
+      toggleMenu() {
+        this.menuOpen = !this.menuOpen;
+      },
+    },
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-@import "../assets/_variables.scss";
+  @import "../assets/_variables.scss";
 
-.menu-to-slide {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  z-index: 999;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  right: -223px;
-  transition: right 0.5s ease;
-  box-shadow: -2px 0 3px #80808047;
-  &.active {
-    right: 0;
+  .menu-to-slide {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    z-index: 999;
+    position: fixed;
     top: 0;
     bottom: 0;
+    right: -223px;
     transition: right 0.5s ease;
+    box-shadow: -2px 0 3px #80808047;
+    &.active {
+      right: 0;
+      top: 0;
+      bottom: 0;
+      transition: right 0.5s ease;
+      .menu-button {
+        .menu-icon {
+          transition: left 0.5s ease, transform 5s;
+          left: 32px;
+          &:hover {
+            transform: rotate(180deg);
+            transform-origin: 50% 50%;
+            transition: transform 5s, left 0.5s ease;
+          }
+        }
+      }
+    }
     .menu-button {
+      width: 48px;
+      height: 96px;
+      display: flex;
+      flex-direction: column;
+      position: absolute;
+      left: -48px;
+      cursor: pointer;
+      .hex {
+        height: 96px;
+        width: 97px;
+        position: absolute;
+        filter: drop-shadow(-2px 0 2px #29292947);
+        filter: hue-rotate(168deg);
+      }
       .menu-icon {
+        position: absolute;
+        right: 0;
+        left: 16px;
+        top: 0;
+        bottom: 0;
+        // it is 33 instead of 32 because transform origin point can be exactly in the middle at 50% 50%
+        font-size: 33px;
+        height: 33px;
+        width: 33px;
+        margin: auto;
+        background: -webkit-linear-gradient(left top, #fedb37, #fdb931, #9f7928, #8a6e2f);
+        background: -webkit-linear-gradient(left top, #ac6943, #fbd1c1, #ab6943, #faf4ed);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
         transition: left 0.5s ease, transform 5s;
-        left: 32px;
         &:hover {
           transform: rotate(180deg);
           transform-origin: 50% 50%;
@@ -69,63 +108,24 @@ export default {
       }
     }
   }
-  .menu-button {
-    width: 48px;
-    height: 96px;
+  .menu-list {
+    width: 230px;
+    height: 100%;
+    padding: 20px 20px 20px 52px;
     display: flex;
     flex-direction: column;
-    position: absolute;
-    left: -48px;
-    cursor: pointer;
-    .hex {
-      height: 96px;
-      width: 97px;
-      position: absolute;
-      filter: drop-shadow(-2px 0 2px #29292947);
-      filter: hue-rotate(168deg);
+    box-sizing: border-box;
+    background-color: white;
+    align-items: flex-end;
+    a:last-of-type {
+      margin-bottom: 36px;
     }
-    .menu-icon {
-      position: absolute;
-      right: 0;
-      left: 16px;
-      top: 0;
-      bottom: 0;
-      // it is 33 instead of 32 because transform origin point can be exactly in the middle at 50% 50%
-      font-size: 33px;
-      height: 33px;
-      width: 33px;
-      margin: auto;
-      background: -webkit-linear-gradient(left top, #fedb37, #fdb931, #9f7928, #8a6e2f);
-      background: -webkit-linear-gradient(left top, #ac6943, #fbd1c1, #ab6943, #faf4ed);
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-      transition: left 0.5s ease, transform 5s;
-      &:hover {
-        transform: rotate(180deg);
-        transform-origin: 50% 50%;
-        transition: transform 5s, left 0.5s ease;
+    @media only screen and (max-width: 768px) {
+      flex-direction: column-reverse;
+      a:last-of-type {
+        margin-bottom: 16px;
+        margin-top: 32px;
       }
     }
   }
-}
-.menu-list {
-  width: 230px;
-  height: 100%;
-  padding: 20px 20px 20px 52px;
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
-  background-color: white;
-  align-items: flex-end;
-  a:last-of-type {
-    margin-bottom: 36px;
-  }
-  @media only screen and (max-width: 768px) {
-    flex-direction: column-reverse;
-    a:last-of-type {
-      margin-bottom: 16px;
-      margin-top: 32px;
-    }
-  }
-}
 </style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -7,8 +7,25 @@
       url="https://www.claudiaengelsman.com/about"
     />
     <MenuSlide />
+    <header>
+      <img src="../assets/BlogHeaderNZ.png" alt="close-up of a fern plant in the shade" class="cover" />
+      <div class="intro">
+        <p>Welcome to my portfolio</p>
+        <vue-typed-js
+          :strings="['am proud of', 'am excited to share', 'have learned a lot from']"
+          :loop="true"
+          :loopCount="2"
+          :typeSpeed="75"
+          :backDelay="250"
+          :backSpeed="35"
+          :smartBackspace="true"
+        >
+          <h1>Projects I <span class="typing"></span></h1>
+        </vue-typed-js>
+      </div>
+    </header>
     <img
-      alt="Claudia Engelsman, developer who will teach you how to code a website"
+      alt="Claudia Engelsman, developer who moved to New Zealand"
       src="../assets/ClaudiaBWKaikoura.jpg"
       class="intro-pic img-block"
     />
@@ -20,15 +37,29 @@
       </p>
     </section>
     <img
-      alt="Claudia Engelsman, developer who will teach you how to code a website"
-      src="../assets/ClaudiaBWKaikoura.jpg"
+      alt="Screenshot of the Tower Junction Physio website"
+      src="../assets/TJPScreenshot.jpg"
       class="project-image project1 img-block"
     />
     <section class="project-text project1 text-block">
-      <h3>Hi there!</h3>
+      <h3>Tower Junction Physio (Christchurch)</h3>
       <p>
-        Here you will find my favorite projects. Projects that turned out wonderful, were interesting to built or for
-        really cool people and companies.
+        A specially designed website build with Vuejs to fit all their website needs. Content managed through Prismic.io
+        and hosted on Netlify. Tower Junction Physio were such lovely people to work with and were really happy with the
+        new website that is mobile friendly to view and they can update easily themselves.
+      </p>
+    </section>
+    <img
+      alt="Screenshot of the Bikes and I website"
+      src="../assets/TheBikesAndIScreenshot.jpg"
+      class="project-image project2 img-block"
+    />
+    <section class="project-text project2 text-block">
+      <h3>The Bikes and I</h3>
+      <p>
+        A blog build for a dear friend who is crazy about bikes and loves to share about all things bikes. Website is
+        build with Vuejs and the content is managed through Prismic.io and hosted on Netlify. I did not design this
+        website.
       </p>
     </section>
   </div>
@@ -53,28 +84,80 @@
     display: grid;
     grid-gap: 0;
     grid-template-columns: repeat(2, 50%);
-    grid-template-rows: repeat(3, 500px);
+    grid-template-rows: 300px repeat(2, 500px);
     grid-template-areas:
+      "header header"
       "intro-pic intro-text"
       "project1text project1image"
+      "project2image project2text"
       "outro outro";
     @media only screen and (max-width: 768px) {
       grid-template-columns: repeat(2, 50%);
-      grid-template-rows: auto;
+      grid-template-rows: 300px auto;
       grid-template-areas:
-        "intro-pic intro-pic"
+        "header header"
         "intro-text intro-text"
         "project1image project1image"
         "project1text project1text"
-        "outro outro";
+        "project2image project2image"
+        "project2text project2text";
     }
     .mt-2 {
       margin-top: 20px;
     }
   }
+
+  header {
+    grid-area: header;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    .cover {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      height: 300px;
+      object-fit: cover;
+      width: 100%;
+      max-width: 4000px;
+      object-position: center;
+      margin-left: auto;
+      margin-right: auto;
+      display: block;
+      z-index: 0;
+    }
+    .intro {
+      z-index: 100;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      h1 {
+        font-family: "Karla", sans-serif;
+        display: inline-block;
+        text-align: center;
+      }
+      h2 {
+        font-family: "Spectral", sans-serif;
+        font-size: 37px;
+        line-height: 64px;
+        color: white;
+      }
+      p {
+        color: white;
+        border-bottom: white 2px solid;
+      }
+    }
+  }
+
   .intro-pic {
     grid-area: intro-pic;
     object-position: center;
+    @media only screen and (max-width: 768px) {
+      display: none;
+    }
   }
   .intro-text {
     grid-area: intro-text;
@@ -83,35 +166,20 @@
     &.project1 {
       grid-area: project1text;
     }
+    &.project2 {
+      grid-area: project2text;
+    }
   }
   .project-image {
-    object-position: center;
+    object-position: top left;
     &.project1 {
       grid-area: project1image;
     }
+    &.project2 {
+      grid-area: project2image;
+    }
+  }
 
-    // display: flex;
-    // flex-direction: column;
-    // padding: 80px;
-    // background-color: $dark-blue;
-    // text-align: center;
-    h3 {
-      color: $light-blue;
-    }
-    p {
-      color: $text-darkbg;
-      width: 80%;
-      max-width: 1400px;
-      &.highlight {
-        font-size: 18px;
-        font-style: italic;
-        color: $light-blue;
-        padding: 12px 0;
-        font-weight: 500;
-      }
-    }
-    @media only screen and (max-width: 768px) {
-      padding: 80px 40px;
-    }
+  .img-block {
   }
 </style>
